5a8e89da20ce241de360002fd2906c7bdc611d53
Yathin <yathin_sreeram@epam.com>
2024-08-15 22:41:12 +0530

    Initial commit

---
+++ TableBookingApplication/src/test/java/com/yathin/TableBookingApplication/TableBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yathin.TableBookingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
